4bfb7bbd4775b9d39847ee41e5f511dbe381765e
QuangHao2011 <secretssrt@gmail.com>
2026-06-17 23:10:38 +0700

    Add files via upload

---
+++ writeup/two-complement-code/debugs/test_type.s
@@ -0,0 +1,29 @@
+	.file	"test_type.c"
+	.text
+	.section	.rodata
+	.align 8
+.LC0:
+	.base64	"a28gaOG7o3AgbOG7hywgbOG7l2kgZGnhu4VuIGdp4bqjaQA="
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB0:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	leaq	.LC0(%rip), %rax
+	movq	%rax, %rdi
+	call	puts@PLT
+	movl	$0, %eax
+	popq	%rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	main, .-main
+	.ident	"GCC: (GNU) 16.1.1 20260430"
+	.section	.note.GNU-stack,"",@progbits
